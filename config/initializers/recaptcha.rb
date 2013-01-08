064d8ad80df4af796ffe61e6f8b2bdc285bd443e
Sergio Alonso <i@example>
2013-01-08 11:14:07 -0300

    Correcciones a recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6Lcaw9oSAAAAAF3JksvqjYwADHCFco7JDkOp505R'
-  config.private_key = '6Lcaw9oSAAAAAF3JksvqjYwADHCFco7JDkOp505R'
+  config.public_key  = '6LdqQ9sSAAAAAIwEwPrcaFc3zdE6ZlKzqlH3dQVx'
+  config.private_key = '6LdqQ9sSAAAAALmTTJhSI3Rjeax17HrxzYmYE8Nl'
 #  config.proxy = 'http://myproxy.com.au:8080'
 end
